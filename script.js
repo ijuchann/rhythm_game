e005dc14d0024c5ed561b68231b598861ed3ef2b
ijuchann <104546199+ijuchann@users.noreply.github.com>
2024-11-04 16:04:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('startButton').addEventListener('click', function() {
+    startGame();
+});
+
+function startGame() {
+    // 게임 시작 로직
+    alert('게임이 시작되었습니다!');
+    // 여기에 추가적인 게임 로직을 구현하세요.
+}
